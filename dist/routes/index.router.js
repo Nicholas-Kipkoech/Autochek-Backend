3e1982ca2ba700bc1ecfd43da832f32dcab6f12e
Nicholas-Kipkoech <nickey968@gmail.com>
2022-08-10 18:54:56 +0300

    rm node_modules

--- dist/routes/index.router.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express = require("express");
-const router = express.Router();
-const { lastPosted, frequentWords } = require("../controller/index.controller");
-router.route("/last/posted").get(lastPosted);
-router.route("/words").get(frequentWords);
-module.exports = router;
